f58e282b16e2990a5a8958cffdfdbcc032e4f8d9
Andreyhuey <andreyhuey777@gmail.com>
2022-07-19 11:37:15 +0100

    assets relocated

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
+      <Product />
+      <FooterBanner />
     </>
   );
 };
