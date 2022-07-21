c19cae90e5e88de026cb9e8ac9b856d1f2694532
Andreyhuey <andreyhuey777@gmail.com>
2022-07-21 17:06:04 +0100

    Front Page components completed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiFillGithub,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <p>2022 CultureX All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillGithub />
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
 
-        {/* <Image
-          src="/assets/a64b345016e96adfb8849af5521c8e0ecfe8f027-555x555.webp"
-          width={20}
-          height={20}
+        <img
+          src={urlFor(heroBanner.image)}
           alt="headphones"
           className="hero-banner-image"
-        /> */}
+        />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
